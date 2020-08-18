83b86c31e1fc12d7d0861367efacd47a6e7d0667
Sridhar Krishnamurthy <sridhar.krishnamurthy@sage.com>
2020-08-17 17:06:34 -0700

    Create CI ECR

---
+++ terraform/aws/usw/ci/variables.tf
@@ -0,0 +1,9 @@
+variable "region" { default = "us-west-2" }
+
+variable "auth_profile" { default = "2auth" }
+
+variable "create_repository" { default = false }
+
+variable "repository_name" { default = "ia-devops-repo" }
+
+variable "attach_lifecycle_policy" { default = true }
