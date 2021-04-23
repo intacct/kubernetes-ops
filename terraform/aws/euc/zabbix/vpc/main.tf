4c25ef87ef210219828a4a89e66ceb858bc6dafa
Sridhar Krishnamurthy <sridhar.krishnamurthy@sage.com>
2021-04-23 10:27:51 -0700

    add firewall rule for 10051

--- terraform/aws/euc/zabbix/vpc/main.tf
+++ terraform/aws/euc/zabbix/vpc/main.tf
@@ -132,92 +132,16 @@ locals {
         cidr_block  = "10.235.4.10/32"
       },
       {
-        # Radius requests 10.226.12.13, 10.226.12.14
+        # Radius requests 10.226.12.12, 10.226.12.14
          rule_number = 910
          rule_action = "allow"
          from_port   = 1812
          to_port     = 1812
          protocol    = "-1"
-         cidr_block  = "10.226.12.13/30"
+         cidr_block  = "10.226.12.12/30"
       },
     ]
     default_outbound = [
-      {
-        rule_number = 800
-        rule_action = "allow"
-        icmp_code   = -1
-        icmp_type   = -1
-        protocol    = "icmp"
-        cidr_block  = "0.0.0.0/0"
-      },  
-      {
-        rule_number = 810
-        rule_action = "allow"
-        from_port   = 22
-        to_port     = 22
-        protocol    = "tcp"
-        cidr_block  = "0.0.0.0/0"
-      },
-      {
-        rule_number = 820
-        rule_action = "allow"
-        from_port   = 80
-        to_port     = 80
-        protocol    = "tcp"
-        cidr_block  = "0.0.0.0/0"
-      },
-      {
-        rule_number = 830
-        rule_action = "allow"
-        from_port   = 443
-        to_port     = 443
-        protocol    = "tcp"
-        cidr_block  = "0.0.0.0/0"
-      },
-      {
-        rule_number = 840
-        rule_action = "allow"
-        from_port   = 32768
-        to_port     = 65535
-        protocol    = "tcp"
-        cidr_block  = "0.0.0.0/0"
-      },
-      {
-        # DNS resolution
-        rule_number = 850
-        rule_action = "allow"
-        from_port   = 53
-        to_port     = 53
-        protocol    = "-1"
-        cidr_block  = "0.0.0.0/0"
-      },
-      {
-        # NTP traffic
-        rule_number = 860
-        rule_action = "allow"
-        from_port   = 123
-        to_port     = 123
-        protocol    = "udp"
-        cidr_block  = "0.0.0.0/0"
-      },
-      {
-        # DNS Server requests
-         rule_number = 870
-         rule_action = "allow"
-         from_port   = 0
-         to_port     = 0
-         protocol    = "-1"
-         cidr_block  = "192.168.20.13/32"
-      },
-      {
-        # Radius requests
-         rule_number = 880
-         rule_action = "allow"
-         from_port   = 1812
-         to_port     = 1812
-         protocol    = "-1"
-         cidr_block  = "10.226.12.13/30"
-      },
     ]
     public_inbound = [
       {
@@ -230,6 +154,16 @@ locals {
         protocol    = "tcp"
         cidr_block  = "0.0.0.0/0"
       },
+      {
+        # Port for Zabbix server zabbix-eng
+        rule_number = 200
+        rule_action = "allow"
+        from_port   = 10051
+        to_port     = 10051
+        protocol    = "tcp"
+        cidr_block  = "192.168.20.21/32"
+      },
+
     ]
     public_outbound = [
       {
@@ -238,7 +172,7 @@ locals {
         rule_action     = "allow"
         from_port       = 0
         to_port         = 65535
-        protocol        = "tcp"
+        protocol        = "all"
         cidr_block      = "0.0.0.0/0"
       },
     ]
