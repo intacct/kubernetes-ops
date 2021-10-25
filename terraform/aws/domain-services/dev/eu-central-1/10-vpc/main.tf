b84df794468b991a1e24c978c37877885385a35b
Garland Kan <garland.kan@MacBook-Pro.local>
2021-10-25 13:46:42 -0700

    VPC

---
+++ terraform/aws/domain-services/dev/eu-central-1/10-vpc/main.tf
@@ -0,0 +1,48 @@
+locals {
+  aws_region       = "eu-central-1"
+  environment_name = "dev"
+  tags = {
+    ops_env              = "${local.environment_name}"
+    ops_managed_by       = "terraform",
+    ops_source_repo      = "do-infrastructure",
+    ops_source_repo_path = "terraform/aws/domain-services/${local.environment_name}/eu-central-1/10-vpc",
+    ops_owners           = "devops",
+  }
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.37.0"
+    }
+  }
+
+  backend "remote" {
+    organization = "intacct"
+
+    workspaces {
+      name = "terraform_aws_domain-services_dev_eu-central-1_10-vpc"
+    }
+  }
+}
+
+provider "aws" {
+  region = local.aws_region
+}
+
+#
+# VPC
+#
+module "vpc" {
+  source = "github.com/ManagedKube/kubernetes-ops//terraform-modules/aws/vpc?ref=v1.0.24"
+
+  aws_region       = local.aws_region
+  azs              = ["eu-central-1a", "eu-central-1c", "eu-central-1d"]
+  vpc_cidr         = "10.0.0.0/16"
+  private_subnets  = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets   = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  environment_name = local.environment_name
+  cluster_name     = local.environment_name
+  tags             = local.tags
+}
