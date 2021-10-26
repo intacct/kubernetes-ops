1a80399310e1ddb7d77b452a036f649f9c1b5df0
Garland Kan <garland.kan@macbook-pro.lan>
2021-10-26 15:40:03 -0700

    Updating organization

--- terraform/aws/domain-services/dev/eu-central-1/10-vpc/main.tf
+++ terraform/aws/domain-services/dev/eu-central-1/10-vpc/main.tf
@@ -19,7 +19,7 @@ terraform {
   }
 
   backend "remote" {
-    organization = "intacct"
+    organization = "ia-ds"
 
     workspaces {
       name = "terraform_aws_domain-services_dev_eu-central-1_10-vpc"
