b5482913589287f5864ad2503a8ce09f998b7c2b
Garland Kan <garland.kan@macbook-pro.lan>
2021-11-19 06:48:55 -0800

    Example app 2

---
+++ terraform/aws/domain-services/dev/us-west-2/dev01/helm/example-app-2/main.tf
@@ -0,0 +1,111 @@
+locals {
+  namespace         = "example-app-2"
+  fullnameOverride  = "app2"
+  replica_count     = 1
+  docker_repository = "mendhak/http-https-echo"
+  docker_tag        = "21"
+  requests_memory   = "0.2Gi"
+  local_tags = {
+    ops_source_repo_path = "${var.base_path}/${var.aws_region}/${var.environment_name}/helm/example-app-2",
+  }
+  tags = merge(var.environment_tags, local.local_tags, { "ops_env" : var.environment_name })
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.37.0"
+    }
+    random = {
+      source = "hashicorp/random"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "2.3.0"
+    }
+  }
+
+  backend "remote" {
+    organization = "ia-ds"
+
+    workspaces {
+      name = "ds_dev_us-west-2_dev01_helm_example-app-2"
+    }
+  }
+}
+
+variable "base_path" {}
+variable "base_env_name" {}
+variable "aws_region" {}
+variable "environment_name" {}
+variable "environment_tags" {}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+data "terraform_remote_state" "eks" {
+  backend = "remote"
+  config = {
+    organization = "ia-ds"
+    workspaces = {
+      name = "ds_${var.base_env_name}_${var.aws_region}_${var.environment_name}_20_eks"
+    }
+  }
+}
+
+#
+# EKS authentication
+# # https://registry.terraform.io/providers/hashicorp/helm/latest/docs#exec-plugins
+provider "helm" {
+  kubernetes {
+    host                   = data.terraform_remote_state.eks.outputs.cluster_endpoint
+    cluster_ca_certificate = base64decode(data.terraform_remote_state.eks.outputs.cluster_certificate_authority_data)
+    exec {
+      api_version = "client.authentication.k8s.io/v1alpha1"
+      args        = ["eks", "get-token", "--cluster-name", "${var.environment_name}", "--region", "${var.aws_region}"]
+      command     = "aws"
+    }
+  }
+}
+
+# Helm values file templating
+data "template_file" "helm_values" {
+  template = file("${path.module}/helm_values.yaml")
+
+  # Parameters you want to pass into the helm_values.yaml.tpl file to be templated
+  vars = {
+    fullnameOverride  = local.fullnameOverride
+    namespace         = local.namespace
+    replica_count     = local.replica_count
+    docker_repository = local.docker_repository
+    docker_tag        = local.docker_tag
+    requests_memory   = local.requests_memory
+  }
+}
+
+#
+# Helm - example app
+#
+# Doc: https://github.com/ManagedKube/example-app-2
+module "app" {
+  source = "github.com/ManagedKube/kubernetes-ops//terraform-modules/aws/helm/helm_generic?ref=v1.0.9"
+
+  # this is the helm repo add URL
+  repository = "https://helm-charts.managedkube.com"
+  # This is the helm repo add name
+  official_chart_name = "standard-application"
+  # This is what you want to name the chart when deploying
+  user_chart_name = local.fullnameOverride
+  # The helm chart version you want to use
+  helm_version = "1.0.11"
+  # The namespace you want to install the chart into - it will create the namespace if it doesnt exist
+  namespace = local.namespace
+  # The helm chart values file
+  helm_values = data.template_file.helm_values.rendered
+
+  depends_on = [
+    data.terraform_remote_state.eks
+  ]
+}
