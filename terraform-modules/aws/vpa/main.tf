6b7afa05861dd35b51d7f841e0e8f6351f20463b
ana-ghirghilijiu-sage <ana.ghirghilijiu@sage.com>
2023-06-29 12:44:28 -0500

    [IAOPSPS-1161] updating the module vpa

--- terraform-modules/aws/vpa/main.tf
+++ terraform-modules/aws/vpa/main.tf
@@ -1,20 +1,32 @@
+# resource "null_resource" "vpa_deployment" {
+#   triggers = {
+#     git_clone = module.git_clone.timestamp
+#   }
+
+#   provisioner "local-exec" {
+#     command = <<EOF
+#       git clone https://github.com/kubernetes/autoscaler.git
+#       cd autoscaler/vertical-pod-autoscaler
+#       ./hack/vpa-up.sh
+#     EOF
+#   }
+# }
+
+# module "git_clone" {
+#   source = "cloudposse/git-archive/null"
+#   version = "0.3.2"
+#   repository = var.vpa_repo
+#   output_path = var.output_path
+# }
 resource "null_resource" "vpa_deployment" {
   triggers = {
-    git_clone = module.git_clone.timestamp
+    file_changes = fileset(path.module, "vertical-pod-autoscaler/**")
   }
 
   provisioner "local-exec" {
     command = <<EOF
-      git clone https://github.com/kubernetes/autoscaler.git
-      cd autoscaler/vertical-pod-autoscaler
+      cd vertical-pod-autoscaler
       ./hack/vpa-up.sh
     EOF
   }
-}
-
-module "git_clone" {
-  source = "cloudposse/git-archive/null"
-  version = "0.3.2"
-  repository = var.vpa_repo
-  output_path = var.output_path
 }
