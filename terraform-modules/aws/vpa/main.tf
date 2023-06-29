abd36a2294092ff5aea471246ad31542a393fafb
ana-ghirghilijiu-sage <ana.ghirghilijiu@sage.com>
2023-06-29 16:44:00 -0500

    [IAOPSPS-1161] updating the module vpa

--- terraform-modules/aws/vpa/main.tf
+++ terraform-modules/aws/vpa/main.tf
@@ -2,11 +2,17 @@ resource "null_resource" "vpa_deployment" {
   provisioner "local-exec" {
     command     = "./hack/vpa-up.sh"
     working_dir = "autoscaler/vertical-pod-autoscaler"
+    when        = create
   }
 
   provisioner "local-exec" {
     command     = "./hack/vpa-down.sh"
     working_dir = "autoscaler/vertical-pod-autoscaler"
     when        = destroy
+  }
+
+  # Dummy input variable to trigger re-provisioning
+  input_variables = {
+    trigger = timestamp()
   }
 }
