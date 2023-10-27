b831d74a477649a1f19e0233c472d7937cb66bcf
Prasanth Koya <prasanth.koya@sage.com>
2023-10-27 10:18:20 -0500

    [IAOPSPS-1824]-creating module for s3-nfs-share

---
+++ terraform-modules/aws/s3-nfs-share/output.tf
@@ -0,0 +1,32 @@
+output "ec2_sgw_public_ip" {
+  value       = module.ec2_sgw.public_ip
+  description = "The Public IP address of the created Elastic IP."
+  sensitive   = true
+}
+
+output "ec2_sgw_private_ip" {
+  value       = module.ec2_sgw.public_ip
+  description = "The Private IP address of the Storage Gateway on EC2"
+  sensitive = true
+}
+
+output "storage_gateway" {
+  value       = module.sgw.storage_gateway
+  description = "Storage Gateway Module"
+  sensitive   = true
+}
+
+output "storage_gateway_name" {
+  value       = module.sgw.storage_gateway_name
+  description = "Storage Gateway Name"
+}
+
+output "nfs_share_arn" {
+  value       = module.nfs_share.nfs_share_arn
+  description = "The ARN of the created NFS File Share."
+}
+
+output "nfs_share_path" {
+  value       = module.nfs_share.nfs_share_path
+  description = "The path of the created NFS File Share."
+}
